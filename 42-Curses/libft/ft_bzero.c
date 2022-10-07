3c7146e0bb3f707ee00156739f18423410ad4d53
Omskka <92455486+Omskka@users.noreply.github.com>
2022-10-07 14:52:02 +0300

    Add files via upload

---
+++ 42-Curses/libft/ft_bzero.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 18:01:09 by okirca            #+#    #+#             */
+/*   Updated: 2022/10/07 12:07:03 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*void	ft_bzero(void *s, size_t n)
+{
+	char	*str;
+	size_t	i;
+
+	str = s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
+
+int main()
+{
+	char c[] = "murat";
+	printf("%s", ft_bzero(c, 3));
+	return 0;
+}*/
