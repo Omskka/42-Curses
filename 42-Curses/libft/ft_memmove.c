6f441e3c885962879b4e504439bdb19e305a6760
Omskka <92455486+Omskka@users.noreply.github.com>
2022-10-20 17:33:40 +0300

    Add files via upload

---
+++ 42-Curses/libft/ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 12:37:08 by okirca            #+#    #+#             */
+/*   Updated: 2022/10/08 15:28:04 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*dest;
+	char	*src2;
+	size_t	i;
+
+	i = 0;
+	dest = (char *)dst;
+	src2 = (char *)src;
+	if (!dst && !src)
+		return (NULL);
+	if (src2 < dest)
+		while (++i <= len)
+			dest[len - i] = src2[len - i];
+	else
+		return (ft_memcpy(dst, src, len));
+	return (dst);
+}
---
+++ 42-Curses/libft/ft_split.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 19:54:29 by okirca            #+#    #+#             */
+/*   Updated: 2022/10/20 16:51:42 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	wordcounter(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s == '\0')
+			return (i);
+		while (*s != c && *s)
+			s++;
+		i++;
+	}
+	return (i);
+}
+
+static int	charcounter(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*s && (*s != c))
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ret;
+	int		retindex;
+
+	if (!s)
+		return (0);
+	retindex = 0;
+	ret = malloc(sizeof(char *) * wordcounter(s, c) + 1);
+	if (!ret)
+		return (0);
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s == '\0')
+			break ;
+		ret[retindex] = ft_substr(s, 0, charcounter(s, c));
+		retindex++;
+		s = s + charcounter(s, c);
+	}
+	ret[retindex] = NULL;
+	return (ret);
+}
