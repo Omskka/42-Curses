24ed69df97a31bd8ac8c28f98bfe3716c36d4e76
Omskka <92455486+Omskka@users.noreply.github.com>
2022-10-31 13:48:01 +0300

    Add files via upload

---
+++ libft 2/ft_lstadd_back.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <okirca@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/27 14:06:00 by taydogan          #+#    #+#             */
+/*   Updated: 2022/10/31 13:38:00 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*mlst;
+
+	mlst = *lst;
+	if (!new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		while (mlst->next != NULL)
+		{
+			mlst = mlst->next;
+		}
+		mlst->next = new;
+	}
+}
